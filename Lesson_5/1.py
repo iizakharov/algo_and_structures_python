bbe29fada6bc6e9ad4bf0775d17ff904610aceb8
iizakharov <45385589+iizakharov@users.noreply.github.com>
2019-08-17 14:42:25 +0300

    done

--- Lesson_5/1.py
+++ Lesson_5/1.py
@@ -5,3 +5,28 @@
 вывести наименования предприятий, чья прибыль выше среднего и отдельно
 вывести наименования предприятий, чья прибыль ниже среднего.
 """
+from collections import defaultdict
+
+firm_count = int(input('Введите количество предприятий: '))
+my_dict = defaultdict(list)
+profit_sum = defaultdict(list)
+for index in range(1, firm_count + 1):
+    name = input(f'Введите наименование предприятия {index}: ')
+    for quarter in range(1, 5):
+        my_dict[name].append(float(input(f'Введите прибыль за {quarter} квартал: ')))
+    profit_sum[name] = (sum(my_dict[name]))
+profit_average = sum(profit_sum.values()) / firm_count
+
+print(my_dict)
+print(profit_sum)
+print(f'Средня прибыль по всем предприятиям: {profit_average}')
+
+above_average = []
+below_average = []
+for enterpise in my_dict:
+    if profit_sum[enterpise] > profit_average:
+        above_average.append(enterpise)
+    elif profit_sum[enterpise] < profit_average:
+        below_average.append(enterpise)
+print(f'Предприятия, чья прибыль выше среднего: {", ".join(above_average)}')
+print(f'Предприятия, чья прибыль ниже среднего: {", ".join(below_average)}')
--- Lesson_5/2.py
+++ Lesson_5/2.py
@@ -5,3 +5,19 @@
 [‘C’, ‘4’, ‘F’] соответственно. Сумма чисел из примера: [‘C’, ‘F’, ‘1’],
 произведение - [‘7’, ‘C’, ‘9’, ‘F’, ‘E’].
 """
+
+import collections
+
+num = collections.defaultdict(list)
+for i in range(2):
+    n = input(f'Введите {i+1}-е 16-чное число: ')
+    num[n] = list(n)
+print(num)
+
+summ = 0
+multipl = 1
+for i in num.values():
+    summ += int(''.join(i), 16)
+    multipl *= int(''.join(i), 16)
+print('Сумма: ', list(str(hex(summ))[2:]))
+print('Произведение: ', list(str(hex(multipl))[2:]))
