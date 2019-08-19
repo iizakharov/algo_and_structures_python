caa6c7617bfda5bac8bdac55105e45b02521525d
iizakharov <45385589+iizakharov@users.noreply.github.com>
2019-08-19 15:36:29 +0300

    done

--- Lesson_6/1.py
+++ Lesson_6/1.py
@@ -7,3 +7,49 @@
 комментариев к коду. Также укажите в комментариях версию Python
 и разрядность вашей ОС.
 """
+
+from memory_profiler import profile
+import random
+
+
+@profile
+def cycle():
+    k = 0
+    array = []
+    array_even = []
+
+    for i in range(0, 1000000):
+        array.append(random.randint(1, 20))
+
+    for i in array:
+        if i % 2 == 0:
+            array_even.append(k)
+        k += 1
+
+    print(array)
+    print(array_even)
+    del array
+
+cycle()
+
+# Python 3.7 x64
+
+# Line #    Mem usage    Increment   Line Contents
+# ================================================
+#     14     14.1 MiB     14.1 MiB   @profile
+#     15                             def cycle():
+#     16     14.1 MiB      0.0 MiB       k = 0
+#     17     14.1 MiB      0.0 MiB       array = []
+#     18     14.1 MiB      0.0 MiB       array_even = []
+#     19
+#     20     17.8 MiB      0.0 MiB       for i in range(0, 1000000):
+#     21     17.8 MiB      0.4 MiB           array.append(random.randint(1, 20))
+#     22
+#     23     32.0 MiB      0.1 MiB       for i in array:
+#     24     32.0 MiB      0.0 MiB           if i % 2 == 0:
+#     25     32.0 MiB      0.4 MiB               array_even.append(k)
+#     26     32.0 MiB      0.1 MiB           k += 1
+#     27
+#     28     32.0 MiB      0.0 MiB       print(array)
+#     29     32.0 MiB      0.0 MiB       print(array_even)
+#     30     28.2 MiB      0.0 MiB       del array
--- Lesson_6/2.py
+++ Lesson_6/2.py
@@ -5,3 +5,36 @@
 атрибутов. Сравнить, сколько выделяется памяти для хранения атрибутов обоих
 классов.
 """
+
+from pympler import asizeof
+
+
+class Car:
+    def __init__(self, name):
+        self.name = name
+        self.year = 2019
+        self.volume = 3500
+        self.price = 2500000
+
+
+class Car_slots:
+    __slots__ = ['name', 'year', 'volume', 'price']
+
+    def __init__(self, name):
+        self.name = name
+        self.year = 2019
+        self.volume = 3500
+        self.price = 2500000
+
+
+car1 = Car('Genesis')
+car2 = Car_slots('Genesis')
+
+print(car1.__dict__)
+
+print(asizeof.asizeof(car1))
+print(asizeof.asizeof(car2))
+
+# 312 - без слотов
+# 120 - со слотами
+# Вывод: слоты практически в три раза сэкономили память
