410221ec8450c0bd5e81c299ffea9b8a8ae5f3f0
iizakharov <45385589+iizakharov@users.noreply.github.com>
2019-08-22 14:45:26 +0300

    done

--- Lesson_7/3.py
+++ Lesson_7/3.py
@@ -6,3 +6,45 @@
 массива. Но если это слишком сложно, то используйте метод сортировки,
  который не рассматривался на уроках
 """
+
+from random import randint
+
+m = 5
+arr = [randint(0, 100) for _ in range(2*m + 1)]
+print('Исходный массив: ' + str(arr))
+new_arr = arr
+
+
+def cocktail_sort(orig_list):
+    left = 0
+    right = len(orig_list) - 1
+    while left <= right:
+        for i in range(left, right):
+            if orig_list[i] > orig_list[i+1]:
+                orig_list[i], orig_list[i+1] = orig_list[i+1], orig_list[i]
+        right -= 1
+        for i in range(right, left, -1):
+            if orig_list[i-1] > orig_list[i]:
+                orig_list[i], orig_list[i-1] = orig_list[i-1], orig_list[i]
+        left += 1
+    return orig_list
+
+
+print('Отсортированный массив "Шейкером": ' + str(cocktail_sort(new_arr)))
+
+
+def median(orig_list):
+    for i in range(len(orig_list)):
+        l, m, r = 0, 0, 0
+        for j in range(len(orig_list)):
+            if orig_list[j] > orig_list[i]:
+                r += 1
+            elif orig_list[j] < orig_list[i]:
+                l += 1
+            else:
+                m += 1
+        if abs(l - r) <= (m - 1):
+            return orig_list[i]
+
+
+print('Медианой данного массива будет число: ' + str(median(new_arr)))
